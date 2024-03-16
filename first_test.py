9afd017c91b4396850466522926ac88cfb3804df
Grom-Zadira85 <161002599+Grom-Zadira85@users.noreply.github.com>
2024-03-16 21:29:01 +0200

    pushing  from Second branch (#1)
    * change  first_test.py
    
    * Implement sum function

--- first_test.py
+++ first_test.py
@@ -2,3 +2,8 @@ a = input("Enter your name")
 b = input("Enter your last_name")
 c = input("Enter your age")
 d = input("Enter your position")
+def get_age(age):
+    return age
+
+def get_sum(x,y):
+    return x+y
